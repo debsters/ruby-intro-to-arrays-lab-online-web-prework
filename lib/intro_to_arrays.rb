4745c93d5fe0fc4668add5401b9f8dd9f0e7dc76
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-03-14 23:44:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,29 +3,29 @@ def instantiate_new_array
 end
 
 def array_with_two_elements
-  
+
 end
 
 def first_element
-  
+
 end
 
 def third_element
-  
+
 end
 
 def last_element
-  
+
 end
 
 def first_element_with_array_methods
-  
+
 end
 
 def last_element_with_array_methods
-  
+
 end
 
 def length
-  
+
 end
